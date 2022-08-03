bbf482c0b051013c50e5144c05e3d531a3fbc987
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-03 15:50:28 -0300

    Atualizado componente Respository
    Criado useEffect que recebe o link (repoLanguage) da API para buscar a linguagem de cada repositório

--- src/components/repository/Repository.jsx
+++ src/components/repository/Repository.jsx
@@ -1,9 +1,27 @@
+// Axios
+import axios from "axios";
 
-const Repository = ({ repoName }) => {
+// React
+import { useEffect, useState } from "react";
+
+const Repository = ({ repoName, repoLink, repoLanguages }) => {
+
+        const [languages, setLanguages] = useState({});
+
+
+    useEffect(() => {
+
+        axios.get(repoLanguages).then(response => {
+            setLanguages(response.data);
+            console.log(response.data);
+        }).catch(error => console.log('Error get language: ', error));
+    }, []);
 
     return (
         <div>
-            <p>{repoName}</p>
+            <p><a href={repoLink} target="_blank">{repoName}</a></p>
+            {console.log(Object.keys(languages))}
+
         </div>
       );
 };
