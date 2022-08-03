a6d56c367f3c94948d0c333c9aa9934ca0a241d7
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-03 14:07:54 -0300

    Importado estilo para o componente Search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,15 +1,18 @@
 // React
-import { useState } from 'react'
+import { useState } from 'react';
+
+// Style
+import './Search.css';
 
 const Search = ({ getUserData }) => {
     
     const [user, setUser] = useState(); // Variável que armazena o nome do usuário
 
     return (
-        <>
+        <div className="search">
             <input type="text" onChange={e => setUser(e.target.value)}/>
-            <button onClick={() => getUserData(user)}>Clique aqui</button>
-        </>
+            <button onClick={() => getUserData(user)}>Search</button>
+        </div>
   );
 };
 
